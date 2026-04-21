1ba883b920fbe4da13a8c8a5044fbe54c956bdb4
ProstoySidr <er.ror.98@mail.ru>
2026-04-21 11:54:32 +0300

    update

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import sys, random, time
+import sys, random
 from PyQt6.QtWidgets import *
 from PyQt6.QtGui import *
 from PyQt6.QtCore import *
 
 class MainWindow(QMainWindow):
+
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Цепляясь")
@@ -17,11 +18,11 @@ class MainWindow(QMainWindow):
         self.clear_window()
         label = QLabel("Цепляясь", self)
         Start_Menu_Button = QPushButton("Начать новую игру", self)
-        Start_Menu_Button.clicked.connect(self.gui_screen_Start)
+        Start_Menu_Button.clicked.connect(self.ui_Start)
         Load_Menu_Button = QPushButton("Загрузить", self)
-        Load_Menu_Button.clicked.connect(self.gui_screen_Load)
+        Load_Menu_Button.clicked.connect(self.ui_Load)
         Settings_Menu_Button = QPushButton("Настройки", self)
-        Settings_Menu_Button.clicked.connect(self.gui_screen_Settings) 
+        Settings_Menu_Button.clicked.connect(self.ui_Settings) 
         Exit_Button = QPushButton("Выйти", self)
         Exit_Button.clicked.connect(self.Exit_Button_event)
         self.vbox.addWidget(label, alignment=Qt.AlignmentFlag.AlignCenter)
@@ -37,41 +38,19 @@ class MainWindow(QMainWindow):
             if widget is not None:
                 widget.deleteLater()
 
-    def gui_screen_Settings(self):
-        self.clear_window()
-        ToMain_Button = QPushButton("Назад")
-        ToMain_Button.clicked.connect(self.gui_main_window)
-        self.vbox.addWidget(ToMain_Button, alignment=Qt.AlignmentFlag.AlignCenter)
-
-    def gui_screen_Start(self):
+    def ui_Start(self):
         self.clear_window()
         label = QLabel("Выберите начальный предмет")
         Item_Pick_List = QComboBox()
         Item_Pick_List.setPlaceholderText("Выбери начальный предмет")
-        Item_Pick_List.addItems(
-            ["Ничего", "Броня", "Оружие", "Хлеб"]
-            )
+        Item_Pick_List.addItems(["Ничего", "Броня", "Оружие", "Хлеб"])
         Item_Pick_Button = QPushButton("Выбрать предмет")
         current_item_index = Item_Pick_List.currentIndex()
         current_item = "Ничего"
         inventory = []
-        def current_item_change(self):
-            print(Item_Pick_List.currentIndex())
-            
-        Item_Pick_List.currentIndexChanged.connect(current_item_change)
-        def start(self):
-            if current_item_index == 0:
-                inventory.append("Ничего")
-            elif current_item_index == 1:
-                inventory.append("Броня")
-            elif current_item_index == 2:
-                inventory.append("Оружие")
-            elif current_item_index == 3:
-                inventory.append("Хлеб")
-            print(inventory)
-        
-            self.gui_main_window
-        # Продолжать тут
+        def start():
+                inventory.append(Item_Pick_List.itemText(Item_Pick_List.currentIndex()))
+                self.ui_Game()
         Item_Pick_Button.clicked.connect(start)
         ToMain_Button = QPushButton("Назад")
         ToMain_Button.clicked.connect(self.gui_main_window)
@@ -80,7 +59,7 @@ class MainWindow(QMainWindow):
         self.vbox.addWidget(Item_Pick_Button, alignment=Qt.AlignmentFlag.AlignCenter)
         self.vbox.addWidget(ToMain_Button, alignment=Qt.AlignmentFlag.AlignCenter)
 
-    def gui_screen_Load(self):
+    def ui_Load(self):
         self.clear_window()
         label = QLabel("Выберите сохранение (В разработке)")
         ToMain_Button = QPushButton("Назад")
@@ -88,8 +67,29 @@ class MainWindow(QMainWindow):
         self.vbox.addWidget(label, alignment=Qt.AlignmentFlag.AlignCenter)
         self.vbox.addWidget(ToMain_Button, alignment=Qt.AlignmentFlag.AlignCenter)
 
+    def ui_Settings(self):
+        self.clear_window()
+        ToMain_Button = QPushButton("Назад")
+        ToMain_Button.clicked.connect(self.gui_main_window)
+        self.vbox.addWidget(ToMain_Button, alignment=Qt.AlignmentFlag.AlignCenter)
+
     def Exit_Button_event(self):
         self.close()
+
+    def ui_Game(self):
+        self.clear_window()
+        hbox = QHBoxLayout()
+        Inventory_Button = QPushButton()
+        Map_Button = QPushButton()
+        Inventory_Button.setIcon(QIcon('resources/Backpack_Icon.png'))
+        Map_Button.setIcon(QIcon('resources/Map_Icon.png'))
+        Inventory_Button.setMinimumSize(150, 150)
+        Inventory_Button.setIconSize(QSize(150, 150))
+        Map_Button.setMinimumSize(150, 150)
+        Map_Button.setIconSize(QSize(150, 150))
+        hbox.addWidget(Inventory_Button, alignment=Qt.AlignmentFlag.AlignBottom | Qt.AlignmentFlag.AlignLeft)
+        hbox.addWidget(Map_Button, alignment=Qt.AlignmentFlag.AlignBottom | Qt.AlignmentFlag.AlignRight)
+        self.vbox.addLayout(hbox)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
